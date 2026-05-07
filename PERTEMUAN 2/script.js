956e285175349ff9d23c4204da0388cf10d975cc
rahmatrenaldev <rahmatrenaldi101003@gmail.com>
2026-05-07 19:22:51 +0800

    menambahkan tugas javascript dom

---
+++ PERTEMUAN 2/script.js
@@ -0,0 +1,5 @@
+function sapaUser() {
+    let nama = document.getElementById("nama").value;
+    document.getElementById("hasil").innerHTML =
+        "Halo, " + nama + ". Selamat datang di website saya.";
+}
