262b5d780ed2c2b0c4672940c3d47d2f31d84e11
rahmatrenaldev <rahmatrenaldi101003@gmail.com>
2026-05-07 19:35:53 +0800

    Delete PERTEMUAN 2/script.js

--- PERTEMUAN 2/script.js
+++
@@ -1,5 +0,0 @@
-function sapaUser() {
-    let nama = document.getElementById("nama").value;
-    document.getElementById("hasil").innerHTML =
-        "Halo, " + nama + ". Selamat datang di website saya.";
-}
