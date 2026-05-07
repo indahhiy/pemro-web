8df75b66d6e5dde2c94e11a37053e1ba6ecbd901
Alisiacello <celloalisia@gmail.com>
2026-05-07 17:47:06 +0700

    Update script.js

--- pertemuan_2/javascript-dom/script.js
+++ pertemuan_2/javascript-dom/script.js
@@ -1,1 +1,21 @@
+const formSapa = document.getElementById("formSapa");
+const namaUser = document.getElementById("namaUser");
+const sapaan = document.getElementById("sapaan");
 
+formSapa.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const nama = namaUser.value;
+
+    if (nama === "") {
+
+        sapaan.innerHTML = "Nama tidak boleh kosong!";
+
+    } else {
+
+        sapaan.innerHTML = "<marquee>Halo, " + nama + " 😋 </marquee>";
+
+    }
+
+});
