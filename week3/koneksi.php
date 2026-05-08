df3c1ae40f70c9903a24d26bd92c60736982dada
sucinovilia26 <sucijpr398@gmail.com>
2026-05-08 17:39:35 +0700

    Update koneksi.php

--- week3/koneksi.php
+++ week3/koneksi.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "pweb_weeek3";
 
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
