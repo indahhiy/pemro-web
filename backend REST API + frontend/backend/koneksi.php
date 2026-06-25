4a6d55d7d7f16eb95e8af8c85af5f0ac64de926c
Alisiacello <celloalisia@gmail.com>
2026-06-25 15:37:33 +0700

    Update koneksi.php

--- backend REST API + frontend/backend/koneksi.php
+++ backend REST API + frontend/backend/koneksi.php
@@ -1,1 +1,13 @@
+<?php
 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_perpustakaan";
+
+$con = mysqli_connect($host,$user,$pass,$db);
+
+if(!$con){
+    die("Koneksi gagal");
+}
+?>
